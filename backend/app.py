d146205339b198611840796f4d4a61114ac8938e
Alexa Garcia <alexa.garcia.nunez@gmail.com>
2026-07-14 12:49:53 +0100

    Final reuirments txt

--- backend/app.py
+++ backend/app.py
@@ -274,4 +274,5 @@ def profile():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
